bb241e5a6043167077672aa87d3617d2d90aafb8
Rafael Molina <rafael@molina-aquino.com>
2021-10-01 17:26:15 -0400

    add abis and address

--- src/contracts/stake.js
+++ src/contracts/stake.js
@@ -1,5 +1,6 @@
 import web3 from "../web3";
 
+////address 0xFD471836031dc5108809D173A067e8486B9047A3
 const address = ""; // Please add the address of the deployed contract
 const abi = []; // Plea add the abi of the deployed contract 
 
